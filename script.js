fd27ca7b3ae1397b830e4d2597bd64ec0fcaa025
Jibran Sheikh <jibransheikh.work@gmail.com>
2026-05-26 14:11:36 +0300

    image update

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    AOS.init({
+        duration: 800,
+        easing: 'ease-in-out',
+        once: true,
+        offset: 50
+    });
+    
+    console.log("%c JS. %c BINBOI SYSTEMS OPERATIONAL ", "background:#fff; color:#000; font-weight:bold; padding:2px 5px;", "background:#fb923c; color:#fff; font-weight:bold; padding:2px 5px;");
+});
